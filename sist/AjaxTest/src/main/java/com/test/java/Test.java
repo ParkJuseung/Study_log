57ebc2b9dd3cd3ff81794b55c127eefdd0e6af5f
ParkJuseung <judang0322@gmail.com>
2025-03-18 19:03:22 +0900

    03.18

---
+++ sist/AjaxTest/src/main/java/com/test/java/Test.java
@@ -0,0 +1,27 @@
+package com.test.java;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		int[] list = { 100, 200, 300 };
+		
+		for (int n : list) { //이터레이터
+			System.out.println(n);
+		}
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
